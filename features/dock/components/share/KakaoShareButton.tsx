e842913f5dab2f27f911a5c28bcb64ac31c84ebc
caesiumy <dbs2636@gmail.com>
2025-04-21 04:31:48 +0900

    fix: 카카오 공유 버튼의 조건에 템플릿 ID 체크 추가

--- features/dock/components/share/KakaoShareButton.tsx
+++ features/dock/components/share/KakaoShareButton.tsx
@@ -8,7 +8,7 @@ import { toast } from "sonner";
 
 const KakaoShareButton = () => {
   const handleKakaoShare = async () => {
-    if (!window.Kakao.isInitialized()) {
+    if (!window.Kakao.isInitialized() || !data.share.templateId) {
       toast.error("카카오톡 SDK가 초기화되지 않았습니다.");
       return;
     }
@@ -24,6 +24,7 @@ const KakaoShareButton = () => {
         variant="ghost"
         className="flex flex-col items-center justify-center gap-2 p-12"
         onClick={handleKakaoShare}
+        disabled={!data.share.templateId}
       >
         <MessageCircle size={18} />
         카톡으로 공유
